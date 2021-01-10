6b91117bb746e7a995651687d22eb443bc78b3cf
“Allen <shanmon7799@gmail.com>
2021-01-11 00:17:48 +0800

    Add : fetch test xml from github

---
+++ app/Http/Controllers/GoogleMerchantController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Http\Service\GoogleMerchant;
+
+class GoogleMerchantController extends Controller
+{
+    protected $googleMerchant;
+
+    public function __construct(GoogleMerchant $googleMerchant)
+    {
+        $this->googleMerchantSer = $googleMerchant;
+    }
+
+    public function index()
+    {
+        $xml = $this->fetchTestXml();
+    }
+
+    public function fetchTestXml()
+    {
+        $url = 'https://gist.githubusercontent.com/chivincent-rosetta/0ff2bc5101d391a47a3741fdffa17c3f/raw/658c627b037ed60322e3123af329042a01dff319/product_feed.xml';
+        $xml = simplexml_load_file($url);
+
+        return $xml;
+    }
+}
